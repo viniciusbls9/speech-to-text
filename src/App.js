92d6dcad8bd2b5d306dce229af6cc03834d4cb1d
Vinicius Benedito <beneditoloura@gmail.com>
2020-08-17 23:32:20 -0300

    Aplicação para auxiliar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = 'pt-BR';
+
 function App() {
+
+  const [isListening, setIsListening] = useState(false);
+  const [note, setNote] = useState(null);
+  const [savedNotes, setSavedNotes] = useState([]);
+
+  useEffect(() => {
+    handleListen();
+  }, [isListening]);
+
+  const handleListen = () => {
+    if(isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log('continue...');
+        mic.start();
+      }
+    } else {
+      mic.stop();
+      mic.onend = () => {
+        console.log('stopped Mic on Click');
+      }
+    }
+    mic.onstart = () => {
+      console.log('Mics on');
+    }
+
+    mic.onresult = event => {
+      const transcript = Array.from(event.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('')
+      console.log(transcript);
+      setNote(transcript);
+      mic.onerror = event => {
+        console.log(event.error)
+      }
+
+    }
+  }
+
+  const handleSaveNotes = () => {
+    setSavedNotes([...savedNotes, note]);
+    setNote('')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Fala dos professores</h1>
+      <div className="container">
+        <div className="box">
+          <h2>Só falar que ele aparece na tela</h2>
+          {isListening ? <span>🎤</span> : <span>🔴🎤</span>}
+          <button onClick={handleSaveNotes} disabled={!note}>Salvar</button>
+          <button onClick={() => setIsListening(prevState => !prevState)}>
+            Iniciar / Pausar
+          </button>
+          <p>{note}</p>
+        </div>
+
+        <div className="box">
+          <h2>Anotações salvas</h2>
+          {savedNotes.map(n => (
+            <p key={n}>{n}</p>
+          ))}
+        </div>
+
+      </div>
+    </>
   );
 }
 
